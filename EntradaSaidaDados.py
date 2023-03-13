44500ff1b084b0e2091946ec0e08626a5f4c14d9
Alex Sandro <aleexsandro65@gmail.com>
2023-03-13 20:04:11 -0300

    first commit

---
+++ EntradaSaidaDados.py
@@ -0,0 +1,19 @@
+'''Crie um programa que receba dois números inteiros 
+e depois mostre o resultado da soma e da multiplicação,
+de um número pelo ourto.'''
+
+# escreva dois números tipo inteiro
+
+numero1 = int(input("Informe um número"))
+numero2 = int(input("Informe outro número"))
+
+# Depois mostre o resultado da soma
+# E da multiplicação, dos dois números informados.
+
+resultado_soma = numero1 + numero2
+resultado_multiplicacao = numero1 * numero2
+
+# Printa na tela o resultado
+
+print ("A soma foi:", resultado_soma, "E a multiplicação foi:", resultado_multiplicacao)
+
